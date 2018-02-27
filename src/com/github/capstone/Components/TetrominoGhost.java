99306cfab621249ab9060160b7767fc92f61d4ed
oitsjustjose <stovallj1995@gmail.com>
2018-02-27 09:38:35 -0500

    Revert "Merge branch 'master' of https://github.com/oitsjustjose/MOCS-Arcade-Game"
    This reverts commit 8e6515e8f334f3b485bb70b0a17191f4f0448333, reversing
    changes made to d9e9a2deda23c1f8db03cff08fed6187eb779ef0.

--- src/com/github/capstone/Components/TetrominoGhost.java
+++ src/com/github/capstone/Components/TetrominoGhost.java
@@ -3,7 +3,6 @@ package com.github.capstone.Components;
 import com.github.capstone.Util.Helper;
 import org.lwjgl.opengl.Display;
 import org.newdawn.slick.TrueTypeFont;
-import org.newdawn.slick.opengl.TextureImpl;
 
 public class TetrominoGhost extends Tetromino
 {
@@ -67,6 +66,9 @@ public class TetrominoGhost extends Tetromino
     @Override
     public void draw()
     {
+        // Draw some text:
+        font.drawString(this.getHitBox().getX() - font.getWidth("Next Piece:") - 8, 16, "Next Piece:");
+
         for (TetrominoPiece[] column : super.getPieceMatrix())
         {
             for (TetrominoPiece row : column)
@@ -77,8 +79,6 @@ public class TetrominoGhost extends Tetromino
                 }
             }
         }
-        TextureImpl.bindNone();
-        font.drawString(this.getHitBox().getX() - font.getWidth("Next Piece:") - 8, 16, "Next Piece:");
     }
 /**
 @param none 
--- src/com/github/capstone/Scene/Menus/MainMenu.java
+++ src/com/github/capstone/Scene/Menus/MainMenu.java
@@ -5,6 +5,13 @@ import com.github.capstone.Scene.Game;
 
 public class MainMenu extends Menu
 {
+    /**
+     * @param title The String include for the title.
+     * @return none
+     * @throws none
+     * @Menu This constructor method adds button to a linkedhashmap, and creates a titlesprite.
+     */
+
     public MainMenu()
     {
         super("gui/title");
